d8f4bdc307e6e5da4002eaf9a1a4aa379f112512
gabriellepascual <b2029gjlpascual@pshs.edu.ph>
2026-01-20 22:15:30 +0800

    work on mobilee

--- public/discography.js
+++ public/discography.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
   const container = document.getElementById('albums');
   const buttons = document.querySelectorAll('.layout-switcher [data-layout]');
-
+  const albums = document.querySelectorAll('#albums .album');
+  
   buttons.forEach((btn) => {
     btn.addEventListener('click', () => {
       buttons.forEach(b => b.classList.remove('is-active'));
@@ -9,6 +10,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
       container.classList.remove('album--grid', 'album--vinyl');
       container.classList.add(`album--${btn.dataset.layout}`);
+    });
+  });
+
+  albums.forEach(album => {
+    album.addEventListener('click', () => {
+      if (container.classList.contains('album--vinyl')) {
+        albums.forEach(a => a.classList.remove('active'));
+        album.classList.add('active');
+      }
     });
   });
 });
